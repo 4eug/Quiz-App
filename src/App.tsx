fb2c98e2367a5c8cc63a65b0e928b695195d6e98
Eugene Debrah <debraheug@gmail.com>
2022-04-07 08:52:41 +0000

    Displaying Questions

--- src/App.tsx
+++ src/App.tsx
@@ -48,19 +48,26 @@ function App() {
           Start
         </button>
       ) : null}
-      <p className="score">Score:</p>
-      <p>Loading Questions ....</p>
-      {/* <QuestionCard
-        questionNr={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+      {!gameOver ? <p className="score">Score:</p> : null}
+      {loading && <p>Loading Questions ....</p>}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 }
