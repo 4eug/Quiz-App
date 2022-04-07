93edd4e3b2224097b484c6e892db9bf4370ef320
Eugene Debrah <debraheug@gmail.com>
2022-04-07 08:58:32 +0000

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,18 @@ function App() {
     setLoading(false);
   };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      // Users answer
+      const answer = e.currentTarget.value;
+      // Check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+      // Add score if answer is correct
+      if (correct) setScore((prev) => prev + 1);
+      // Save answer in the array for user answers
+      const answerObject = { question: questions[number].question };
+    }
+  };
 
   const nextQuestion = () => {};
 
